b0afca22440e89f744b1a9fd05043d9ee97423b3
Butsuk Danila <butsukdanila@gmail.com>
2024-09-18 19:43:28 +0700

    add readme & helper script

---
+++ build-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pushd $(dirname $(realpath 0)) > /dev/null
+  [ ! -f ./.build/rtsp-ts-viewer ] && {
+    mkdir -p \.build
+    pushd ./.build > /dev/null
+      cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 "$@"
+      cmake --build . -j $(nproc --all)
+    popd > /dev/null
+  }
+  [ ! -f ./.build/rtsp-ts-viewer ] && {
+    echo "Build failed :(" >& 2
+    exit 1
+  }
+  ./.build/rtsp-ts-viewer "$@"
+popd > /dev/null
