40a2aa6c5901da1c8ebaf7823affb28812f3fa86
Butsuk Danila <butsukdanila@gmail.com>
2024-09-18 19:52:52 +0700

    fix helper script

--- build-run.sh
+++ build-run.sh
@@ -4,7 +4,7 @@ pushd $(dirname $(realpath 0)) > /dev/null
   [ ! -f ./.build/rtsp-ts-viewer ] && {
     mkdir -p \.build
     pushd ./.build > /dev/null
-      cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 "$@"
+      cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1
       cmake --build . -j $(nproc --all)
     popd > /dev/null
   }
